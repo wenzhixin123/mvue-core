30c5e53a08fcfa74e4e0b85d92c8610cf2356090
贺嘉文 <hejw@bingosoft.net>
2018-10-17 14:07:46 +0800

    toPage操作类型 支持弹出模式,toPage跳入页面默认带上dataId数据id,entity实体id

--- src/components/meta_operation/to_page_operation.vue
+++ src/components/meta_operation/to_page_operation.vue
@@ -1,13 +1,39 @@
 <template>
-<div @click.stop.prevent="gotoPage">
-    <slot>
-        <Button type="primary" size="small" 
-            :title="operation.title" >
-            <Icon :type="operation.icon"></Icon>
-            {{operation.title}}
-        </Button>
-    </slot>
-</div>
+    <div>
+        <!--直接跳转-->
+        <div @click.stop.prevent="gotoPage" v-if="!operation.isPopup">
+            <slot>
+                <Button type="primary" size="small"
+                        :title="operation.title" >
+                    <Icon :type="operation.icon"></Icon>
+                    {{operation.title}}
+                </Button>
+            </slot>
+        </div>
+
+
+        <!--打开新窗口模式-->
+        <div @click.stop.prevent="toggleModal" v-if="operation.isPopup">
+            <slot>
+                <Button type="primary" size="small"
+                        :title="operation.title" >
+                    <Icon :type="operation.icon"></Icon>
+                    {{operation.title}}
+                </Button>
+            </slot>
+        </div>
+        <Modal class="popup-widget-con" v-model="popupWidgetModal"
+               :width="modalWidth"
+               :title="modalTitle"
+               :scrollable="true"
+               :mask-closable="false"
+        >
+            <div class="modal-inner-widget" :style="{height:modalHeight+'px'}">
+                <meta-widget-page :widget-params="pageParams"></meta-widget-page>
+            </div>
+            <div slot="footer"></div>
+        </Modal>
+    </div>
 </template>
 <script>
 export default {
@@ -29,19 +55,61 @@ export default {
             });
         }
         return {
-
+            modalWidth:this.operation.modalWidth||500,
+            modalHeight:this.operation.modalHeight||340,
+            modalTitle:this.operation.page.title,
+            popupWidgetModal:false,
+            pageParams:{}
         };
     },
     methods:{
+        getIdFromContext(){
+            var context = Object.assign(this.widgetContext, this.operation);
+            var id = context.selectedId;
+            var metaEntity = context.metaEntity;
+            if(!context.selectedItem&&context.selectedItems){
+                //按钮放置的是在工具栏
+                context.selectedItem = context.selectedItems[(context.selectedItems.length-1)]
+                context.selectedId = context.selectedItem.id;
+                id = context.selectedId;
+            }
+            if (!id&&context.selectedItem) {
+                var selectedItem = context.selectedItem;
+                if (selectedItem) {
+                    //计算id字段
+                    var idField = null;
+                    if (!_.isEmpty(metaEntity)) {
+                        idField = metaEntity.getIdField();
+                    }
+                    id = selectedItem[idField];
+                }
+            }//获取传入的对象id和实体信息
+            return {dataId:id,entity:metaEntity.metaEntityId};
+
+        },
         gotoPage(){
             if(!this.operation.page){
                 return;
             }
-            var _query=_.extend({},this.operation.queryParams);
+            //所有跳转都带入dataId数据id,entity实体id
+            var _query=_.extend({},this.getIdFromContext(),this.operation.queryParams);
             var pageId=this.operation.page.id;
             var _params=_.extend({pageId:pageId},this.operation.pathParams);
             router.push({name:"defaultPageIndex",query:_query,params:_params});
             this.$emit("triggered","toPage");
+        },
+        toggleModal(){
+            if(!this.operation.page){
+                return;
+            }
+            Object.assign(this.$route.query,this.getIdFromContext());
+            this.pageParams = Object.assign({pageId:this.operation.page.id},this.getIdFromContext());
+            this.popupWidgetModal=!this.popupWidgetModal;
+            this.$emit("triggered","popup");
+        },
+        close(){//关闭对话框
+            this.pageParams = {pageId :""};
+            this.popupWidgetModal=false;
         }
     }
 }

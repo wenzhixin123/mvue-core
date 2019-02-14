2cf3f1f2b3292100d482afdb111ef01dcceb06b7
李晶 <lijing@bingosoft.net>
2019-02-14 17:14:16 +0800

    getOptionsExData增加数字0为选项id的处理逻辑

--- src/components/form/js/control_base.js
+++ src/components/form/js/control_base.js
@@ -99,7 +99,7 @@ export default {
     methods:{
         //通过选项id获取选项字段类型数据的显示文字，供显示使用
         getOptionsExData(optionId,cascade){
-            if(!optionId){
+            if(!optionId&&optionId!==0){
                 return '';
             }
             var metaEntity=this.context.metaEntity;

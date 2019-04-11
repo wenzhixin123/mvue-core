90ce906d7f32ad08222851fddce7723276f1e8aa
Bingor <xiaobinbingor@163.com>
2019-04-11 10:47:59 +0800

    撤销metaform.vue的修改

--- src/components/form/metaform.vue
+++ src/components/form/metaform.vue
@@ -1,6 +1,5 @@
 <template>
     <div v-if="preprocessed" class="meta-form-panel" :class="{'has-buttons':hasButtons()}">
-        <div>sssssssss</div>
         <div style="height:30px;position:relative;" v-show="loadingFormData">
             <Spin fix>
             </Spin>

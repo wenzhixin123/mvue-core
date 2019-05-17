ddf574772ea4a53c0e76eed98ab253001871f496
李晶 <lijing@bingosoft.net>
2019-05-17 14:13:22 +0800

    单行文本支持前置后置文本和图标设置

--- src/components/form/control_tmpl/single_line_text.vue
+++ src/components/form/control_tmpl/single_line_text.vue
@@ -4,7 +4,19 @@
                 <div class="form-item-view" v-text="value||emptyText" :title="value||emptyText"></div>
         </template>
         <template v-else>
-            <Input ref="focusInput" v-model="valueObj" @input="updateValue"  :disabled="disabled" type="text"  :placeholder="formItem.componentParams.placeholder"></Input>
+            <Input ref="focusInput" v-model="valueObj" 
+                @input="updateValue" 
+                :prefix="formItem.componentParams.prefix" 
+                :suffix="formItem.componentParams.suffix" 
+                :disabled="disabled" type="text"  
+                :placeholder="formItem.componentParams.placeholder">
+                <Select v-if="formItem.componentParams.prepend" @on-change="updateValue" v-model="prependVal" slot="prepend" :style="{width:formItem.componentParams.prependWidth||'80px'}">
+                    <Option v-for="opt in prependOptions(formItem.componentParams.prepend)" :value="opt" :key="opt">{{opt}}</Option>
+                </Select>
+                <Select v-if="formItem.componentParams.append" @on-change="updateValue" v-model="appendVal" slot="append" :style="{width:formItem.componentParams.appendWidth||'80px'}">
+                    <Option v-for="opt in appendOptions(formItem.componentParams.append)" :value="opt" :key="opt">{{opt}}</Option>
+                </Select>
+            </Input>
         </template>
     </div>
 </template>
@@ -18,13 +30,21 @@ export default {
         }
     },
     data: function(){
+        let {valueObj,prependVal,appendVal}=this.convertedValue();
         return {
-            valueObj:this.value
+            valueObj:valueObj,
+            prependVal:prependVal,
+            appendVal:appendVal
         };
     },
     watch:{
       "value":function (newV,oldV) {
-          this.valueObj=newV;
+          if(_.isEqual(newV,oldV)){
+            let {valueObj,prependVal,appendVal}=this.convertedValue();
+            this.valueObj=valueObj;
+            this.prependVal=prependVal;
+            this.appendVal=appendVal;
+          }
       }
     },
     mounted:function(){
@@ -36,8 +56,60 @@ export default {
         }
     },
     methods: {
-        updateValue: function ($event) {
-            this.$emit('input',this.valueObj);
+        convertedValue(){
+            let res= {
+                valueObj:this.value,
+                prependVal:'',
+                appendVal:''
+            };
+            if(!this.value){
+                return res;
+            }
+            if(this.formItem.componentParams.prepend){
+                let pOptions=this.prependOptions(this.formItem.componentParams.prepend);
+                _.each(pOptions,opt=>{
+                    if(_.startsWith(this.value,opt)){
+                        res.prependVal=opt;
+                        res.valueObj=this.value.substring(opt.length);
+                        return false;
+                    }
+                });
+            }
+            if(this.formItem.componentParams.append){
+                let aOptions=this.appendOptions(this.formItem.componentParams.append);
+                _.each(aOptions,opt=>{
+                    if(_.endsWith(this.value,opt)){
+                        res.appendVal=opt;
+                        res.valueObj=res.valueObj.substring(0,res.valueObj.length-opt.length);
+                        return false;
+                    }
+                })
+            }
+            return res;
+        },
+        updateValue: function () {
+            let res=this.valueObj;
+            if(this.valueObj){
+                res=`${this.prependVal}${res}${this.appendVal}`;
+            }
+            this.$emit('input',res);
+        },
+        prependOptions(val){
+            let valMap={
+                url:['http://','https://']
+            }
+            if(_.isString(val)){
+                return valMap[val]||[];
+            }else if(_.isArray(val)){
+                return val;
+            }
+            return [];
+        },
+        appendOptions(val){
+            if(_.isArray(val)){
+                return val;
+            }
+            return [];
         }
     }
 }

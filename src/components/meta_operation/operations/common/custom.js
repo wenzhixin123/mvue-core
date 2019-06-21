28462c6fcb0c3ef1a81ff2fcbf7bb6a9de47be57
fulsh <fulsh@bingosoft.net>
2019-06-21 13:49:50 +0800

    增加自定义操作默认模板

---
+++ src/components/meta_operation/operations/common/custom.js
@@ -0,0 +1,23 @@
+/**
+ *  列表的创建操作
+ */
+import contextHelper from "../../../../libs/context";
+
+let operation= {
+    name: "custom",
+    title: "自定义",
+    to:null,
+    operationType:"common",
+    btnType:"primary",
+    onclick:function(context,$optInst) {
+        contextHelper.warning("操作未实现");
+    }
+};
+
+
+export default  operation;
+
+
+
+
+

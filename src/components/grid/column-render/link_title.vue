adb448d8147986971b6620388a1e5d52a3c1927b
fulsh <fulsh@bingosoft.net>
2020-02-28 18:47:58 +0800

    发布221版

--- src/components/grid/column-render/link_title.vue
+++ src/components/grid/column-render/link_title.vue
@@ -1,18 +1,19 @@
 <template>
     <m-field v-if="editRow" :name="metaField.name"></m-field>
     <meta-operation v-else  :operation="btn" :widget-context="getWidgetContext()" class="link-title-opt">
-        <Tooltip v-if="initialCol&&initialCol.tooltip" transfer :content="titleVal" :disabled="!showTooltip" :max-width="300" class="ivu-table-cell-tooltip">
-            <a ref="content" @mouseenter="handleTooltipIn" @mouseleave="handleTooltipOut" class="ivu-table-cell-tooltip-content">{{ titleVal }}</a>
+        <Tooltip v-if="initialCol&&initialCol.tooltip" transfer :content="titleValue()" :disabled="!showTooltip" :max-width="300" class="ivu-table-cell-tooltip">
+            <a ref="content" @mouseenter="handleTooltipIn" @mouseleave="handleTooltipOut" class="ivu-table-cell-tooltip-content">{{ htmlValue() }}</a>
         </Tooltip>
-        <a v-else style="display:block;" :class="{'ivu-table-cell-ellipsis':true}" :title="titleVal"  v-html="titleVal"></a>
+        <a v-else style="display:block;" :class="{'ivu-table-cell-ellipsis':true}" :title="titleValue()"  v-html="htmlValue()"></a>
         <template slot="onDeny">
-            <span style="display:block;" :class="{'ivu-table-cell-ellipsis':true}" :title="titleVal"  v-html="titleVal"></span>
+            <span style="display:block;" :class="{'ivu-table-cell-ellipsis':true}" :title="titleValue()"  v-html="htmlValue()"></span>
         </template>
     </meta-operation>
 </template>
 <script>
 import controlTypeService from '../../form/js/control_type_service';
 import batchEditorSupport from './batch-editor-support';
+import constants from '../../form/js/constants';
 export default {
     mixins:[batchEditorSupport],
     props:{
@@ -24,11 +25,11 @@ export default {
             type:Object
         }
     },
-    computed:{
-      titleVal:function () {
-          var val = controlTypeService.formatData(this.item, this.metaField);
-          return val;
-      }
+    computed: {
+        rawTitle: function () {
+            let val = controlTypeService.formatData(this.item, this.metaField);
+            return val;
+        }
     },
     data(){
         return {
@@ -55,7 +56,62 @@ export default {
         },
         handleTooltipOut () {
             this.showTooltip = false;
-        }
+        },
+        hasDeletedTag(){
+            return this.rawTitle&&this.rawTitle.indexOf&&this.rawTitle.indexOf(controlTypeService.entityType.deletedFlag)>-1;
+        },
+        hasTitleSplitKey(){
+            return this.rawTitle&&this.rawTitle.indexOf&&this.rawTitle.indexOf(constants.TitleSplitKey)>-1;
+        },
+        htmlValueForDeletedTag(){
+            let reg = new RegExp(controlTypeService.entityType.deletedFlag,'g');
+            let titles=this.rawTitle.split(',');
+            let t=[];
+            _.each(titles,title=>{
+                if(title.startsWith(controlTypeService.entityType.deletedFlag)){
+                    t.push(`<span class='deleted-ref'>${title.replace(reg,'')}</span>`);
+                }else{
+                    t.push(title);
+                }
+            })
+            return t.join(',');
+        },
+        htmlValueForTitleSplitKey(){
+            let index=this.rawTitle.indexOf(constants.TitleSplitKey);
+            let value=this.rawTitle.substring(0,index);
+            return value;
+        },
+        htmlValue(){
+            //如果有删除标记
+            if(this.hasDeletedTag()){
+                return this.htmlValueForDeletedTag();
+            }else if(this.hasTitleSplitKey()){//如果用title分隔符分开了显示值和标题值
+                return this.htmlValueForTitleSplitKey();
+            }else{
+                return this.rawTitle;
+            }
+        },
+        titleValueForDeletedTag(){
+            let reg = new RegExp(controlTypeService.entityType.deletedFlag,'g');
+            if(this.rawTitle.startsWith(controlTypeService.entityType.deletedFlag)&&this.rawTitle.indexOf(',')<0){
+                return `引用数据${this.rawTitle.replace(reg,'')}已删除`;
+            }
+            return this.rawTitle.replace(reg,'');
+        },
+        titleValueForTitleSplitKey(){
+            let index=this.rawTitle.indexOf(constants.TitleSplitKey);
+            let value=this.rawTitle.substring(index+constants.TitleSplitKey.length);
+            return value;
+        },
+        titleValue(){
+            if(this.hasDeletedTag()){
+                return this.titleValueForDeletedTag();
+            }else if(this.hasTitleSplitKey()){
+                return this.titleValueForTitleSplitKey();
+            }else{
+                return this.rawTitle;
+            }
+        },
     }
 }
 </script>
